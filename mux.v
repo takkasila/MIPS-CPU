e5769d629ba0a9178838874f7cd0dcf9aabe941d
Takkasila <takkasila@outlook.com>
2015-05-14 08:48:14 +0700

    Fixed Mux

--- mux.v
+++ mux.v
@@ -1,4 +1,4 @@
-module mux #(parameter W = 32) (output reg [W-1:0]
+module mux #(parameter W = 32) (output reg [W-1:0] out
 	,input select_in
 	,input [W-1:0] in1 ,input [W-1:0] in2);
 	
@@ -6,10 +6,10 @@ module mux #(parameter W = 32) (output reg [W-1:0]
 		out= 32'h00000000;
 
 		if(select_in == 1) begin
-			out = in1;
+			out = in2;
 		end
 		else begin
-			out = in2;
+			out = in1;
 		end
 	end
 
