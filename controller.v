941cfc968bf80a06c4f7edcd27c990915a14bef6
Takkasila <takkasila@outlook.com>
2015-05-14 12:10:18 +0700

    Started controller

---
+++ controller.v
@@ -0,0 +1,15 @@
+module controller (
+	, output reg RegDst
+	, output reg Branch
+	, output reg MemRead
+	, output reg MemtoReg
+	, output reg MemWrite
+	, output reg ALUSrc
+	, output reg RegWrite
+	, output reg [5:0] ALUSelect_out
+	, input [5:0] OPCode_in
+	, input [5:0] ALUSelect_in);
+
+	
+
+endmodule
