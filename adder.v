b105c470ff5db4afc7f87d0865db4470a893daab
Takkasila <takkasila@outlook.com>
2015-05-15 08:02:22 +0700

    Fixed starting PC address.

--- adder.v
+++ adder.v
@@ -1,6 +1,10 @@
 module adder(output reg [31:0] addr_out
 	, input [31:0] addr_in);
 
+	initial begin
+		addr_out = 32'h003ffffc;
+	end
+
 	always @(addr_in) begin
 		addr_out = addr_in + 4;
 	end
--- program_counter.v
+++ program_counter.v
@@ -2,6 +2,10 @@ module program_counter (output reg [31:0] program_address
 	,input reset, input CLK
 	,input [31:0] in_address);
 
+	initial begin
+		program_address = 32'h003ffffc;
+	end
+
 	always @(posedge CLK) begin
 		if(reset == 1) begin
 			program_address <= 32'h003ffffc;
